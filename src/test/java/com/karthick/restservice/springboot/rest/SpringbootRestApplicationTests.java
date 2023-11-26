f6d2d29e2774ee1a721b88843266dcdb44e6305a
karthee_k <karthickkrishnan24@gmail.com>
2023-11-26 13:47:47 +0530

    Initial commit

---
+++ src/test/java/com/karthick/restservice/springboot/rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthick.restservice.springboot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
